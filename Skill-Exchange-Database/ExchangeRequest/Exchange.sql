ca675a88f2f8577abdfbbb5d5fb071e3698094b7
Bruno Zorima <brunozorima@hotmail.com>
2020-02-25 18:09:52 +0000

    added new fields to the ExchangeRequest and ExchangeMessage tables

--- Skill-Exchange-Database/ExchangeRequest/Exchange.sql
+++ Skill-Exchange-Database/ExchangeRequest/Exchange.sql
@@ -1,7 +1,12 @@
 ﻿CREATE TABLE [dbo].[Exchange]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [Sender_Id] INT NULL, 
-    [Recipient_Id] INT NULL, 
-    [Opened_TimeStamp] TIMESTAMP NULL 
+    [Sender_Id] INT NOT NULL, 
+    [Recipient_Id] INT NOT NULL, 
+	[Status] TINYINT NULL DEFAULT 0,
+    [Opened_TimeStamp] TIMESTAMP NULL,
+	[Last_Message_TimeStamp] TIMESTAMP NULL, 
+    FOREIGN KEY (Sender_Id) REFERENCES [dbo].[ApplicationUser](Id),
+	FOREIGN KEY (Recipient_Id) REFERENCES [dbo].[ApplicationUser](Id)
+
 )
--- Skill-Exchange-Database/ExchangeRequest/ExchangeMessage.sql
+++ Skill-Exchange-Database/ExchangeRequest/ExchangeMessage.sql
@@ -1,8 +1,10 @@
 ﻿CREATE TABLE [dbo].[ExchangeMessage]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [Sender_Id] INT NULL,
-	[Recipient_Id] INT NULL, 
-    [Body] NVARCHAR(MAX) NULL, 
+    [Sender_Id] INT NOT NULL,
+	[Recipient_Id] INT NOT NULL, 
+    [Body] NVARCHAR(MAX) NOT NULL, 
     [TimeStamp] TIMESTAMP NULL, 
+	FOREIGN KEY (Sender_Id) REFERENCES [dbo].[ApplicationUser](Id),
+	FOREIGN KEY (Recipient_Id) REFERENCES [dbo].[ApplicationUser](Id)
 )
