d10eb91f41a15bcaffb28d3ab103a864f22c766b
Bruno Zorima <brunozorima@hotmail.com>
2020-02-25 15:45:29 +0000

    Added new tables: Exchange, Exchange Message

---
+++ Skill-Exchange-Database/ExchangeRequest/Exchange.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Exchange]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Sender_Id] INT NULL, 
+    [Recipient_Id] INT NULL, 
+    [Opened_TimeStamp] TIMESTAMP NULL 
+)
---
+++ Skill-Exchange-Database/ExchangeRequest/ExchangeMessage.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ExchangeMessage]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Sender_Id] INT NULL,
+	[Recipient_Id] INT NULL, 
+    [Body] NVARCHAR(MAX) NULL, 
+    [TimeStamp] TIMESTAMP NULL, 
+)
