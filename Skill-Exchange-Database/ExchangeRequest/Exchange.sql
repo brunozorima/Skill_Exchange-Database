0d2d81953348f143a6e2ce8e6eb424667da6d8a0
Bruno Zorima <brunozorima@hotmail.com>
2020-02-27 17:01:16 +0000

    changed the exchange and exchangeMessage bd by changing the field timestamp to datetime

--- Skill-Exchange-Database/ExchangeRequest/Exchange.sql
+++ Skill-Exchange-Database/ExchangeRequest/Exchange.sql
@@ -1,11 +1,11 @@
 ﻿CREATE TABLE [dbo].[Exchange]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
     [Sender_Id] INT NOT NULL, 
     [Recipient_Id] INT NOT NULL, 
 	[Status] TINYINT NULL DEFAULT 0,
-    [Opened_TimeStamp] TIMESTAMP NULL,
-	[Last_Message_TimeStamp] TIMESTAMP NULL, 
+    [Opened_TimeStamp] DATETIME NULL,
+	[Last_Message_TimeStamp] DATETIME NULL, 
     FOREIGN KEY (Sender_Id) REFERENCES [dbo].[ApplicationUser](Id),
 	FOREIGN KEY (Recipient_Id) REFERENCES [dbo].[ApplicationUser](Id)
 
--- Skill-Exchange-Database/ExchangeRequest/ExchangeMessage.sql
+++ Skill-Exchange-Database/ExchangeRequest/ExchangeMessage.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [dbo].[ExchangeMessage]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
     [Sender_Id] INT NOT NULL,
 	[Recipient_Id] INT NOT NULL, 
     [Body] NVARCHAR(MAX) NOT NULL, 
-    [TimeStamp] TIMESTAMP NULL, 
+    [TimeStamp] DATETIME NULL, 
 	FOREIGN KEY (Sender_Id) REFERENCES [dbo].[ApplicationUser](Id),
 	FOREIGN KEY (Recipient_Id) REFERENCES [dbo].[ApplicationUser](Id)
 )
