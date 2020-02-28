67533d0c28abf8636e5cffb08d9f735b25157ed7
Bruno Zorima <brunozorima@hotmail.com>
2020-02-28 17:03:27 +0000

    added table relationship between exchange and exchange message. also fix the exchangeMessage table.

--- Skill-Exchange-Database/ExchangeRequest/ExchangeMessage.sql
+++ Skill-Exchange-Database/ExchangeRequest/ExchangeMessage.sql
@@ -2,9 +2,9 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
     [Sender_Id] INT NOT NULL,
-	[Recipient_Id] INT NOT NULL, 
+	[Exchange_Id] INT NOT NULL, 
     [Body] NVARCHAR(MAX) NOT NULL, 
     [TimeStamp] DATETIME NULL, 
 	FOREIGN KEY (Sender_Id) REFERENCES [dbo].[ApplicationUser](Id),
-	FOREIGN KEY (Recipient_Id) REFERENCES [dbo].[ApplicationUser](Id)
+	FOREIGN KEY ([Exchange_Id]) REFERENCES [dbo].[Exchange](Id) ON DELETE CASCADE
 )
