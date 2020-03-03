3c4c05978bf64e15b58b6d055e55c0583c3a1d46
Bruno Zorima <brunozorima@hotmail.com>
2020-03-03 18:18:25 +0000

    Change the database structure

--- Skill-Exchange-Database/ExchangeRequest/ExchangeMessage.sql
+++ Skill-Exchange-Database/ExchangeRequest/ExchangeMessage.sql
@@ -3,7 +3,7 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
     [Sender_Id] INT NOT NULL,
 	[Exchange_Id] INT NOT NULL, 
-    [Body] NVARCHAR(MAX) NOT NULL, 
+    [Body] NVARCHAR(400) NOT NULL, 
     [TimeStamp] DATETIME NULL, 
 	FOREIGN KEY (Sender_Id) REFERENCES [dbo].[ApplicationUser](Id),
 	FOREIGN KEY ([Exchange_Id]) REFERENCES [dbo].[Exchange](Id) ON DELETE CASCADE
